2457a23498eff45611942a4fea83a32bf049d680
iUseRod <78047131+iUseRod@users.noreply.github.com>
2021-01-31 09:53:48 +0100

    Added Enchant Lore

--- src/me/helix/atlasenchants/Enchants/IronLungEnchant.java
+++ src/me/helix/atlasenchants/Enchants/IronLungEnchant.java
@@ -2,9 +2,13 @@ package me.helix.atlasenchants.Enchants;
 
 import me.helix.atlasenchants.Main;
 import org.bukkit.Material;
+import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.inventory.InventoryClickEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 public class IronLungEnchant implements Listener {
 
@@ -25,7 +29,24 @@ public class IronLungEnchant implements Listener {
         }
         return false;
     }
-
+    @EventHandler
+    public void onInvChange(InventoryClickEvent e) {
+        if(isHelmet(e.getWhoClicked().getInventory().getHelmet().getType()) == true) {
+        	if(hasCustomEnchant(e.getWhoClicked().getInventory().getHelmet()) == true) {
+        		ItemStack helmet = e.getWhoClicked().getInventory().getHelmet();
+        		ItemMeta HelmetMeta = helmet.getItemMeta();
+                ArrayList<String> lore = new ArrayList<String>();
+                lore.add(Main.color("&cIronLung I"));
+                HelmetMeta.setLore(lore);
+                helmet.setItemMeta(HelmetMeta);
+            	e.getWhoClicked().addPotionEffect(new PotionEffect(PotionEffectType.WATER_BREATHING, Integer.MAX_VALUE, 1));
+        	}
+        }else {
+        	if(e.getWhoClicked().getActivePotionEffects() != null) {
+            	e.getWhoClicked().removePotionEffect(PotionEffectType.WATER_BREATHING);
+        	}
+        }
+    }
     public boolean hasCustomEnchant(ItemStack helmet) {
         ItemMeta helmetMeta = helmet.getItemMeta();
         if (helmetMeta.hasLore()) {
